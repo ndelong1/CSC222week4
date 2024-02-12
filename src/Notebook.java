be72e7e2a344116eff493e4a58c4363caa6d2b25
KotaKat <music9110@gmail.com>
2024-02-12 09:11:57 -0500

    Start of class

--- src/Notebook.java
+++ src/Notebook.java
@@ -2,7 +2,21 @@ import java.util.Scanner;
 
 public class Notebook {
     public static void main(String[] args) {
-        
+        String red = "\u001B[31m";
+        String brightGreen = "\u001B[92m";
+        String blue = "\u001B[34m";
+        String purple = "\u001B[35m";
+        String cyan = "\u001B[36m";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(brightGreen + "Hello World!");
+        System.out.println(red + "We are in Week 4!");
+        System.out.println(cyan + "This is fun!");
+        System.out.print(blue + "What is your name? >> ");
+        String name = input.nextLine();
+
+        System.out.println(purple + "Your name is " + name + ".");
     }
 
 
